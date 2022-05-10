42f2124ba43f6fe2bd13248bdd78b9563ae07abe
gopher_dave <79770840+daviswieck@users.noreply.github.com>
2022-05-09 19:31:22 -0500

    Create const.py

---
+++ custom_components/college-football/const.py
@@ -0,0 +1,22 @@
+# API
+API_ENDPOINT = "http://site.api.espn.com/apis/site/v2/sports/football/college-football/scoreboard"
+USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 11_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.0 Safari/605.1.15"
+
+# Config
+CONF_TIMEOUT = "timeout"
+CONF_TEAM_ID = "team_id"
+
+# Defaults
+DEFAULT_ICON = "mdi:football"
+DEFAULT_NAME = "College-Football"
+DEFAULT_TIMEOUT = 120
+
+# Misc
+TEAM_ID = ""
+VERSION = "0.1"
+ISSUE_URL = "https://github.com/daviswieck/ha-college-football"
+DOMAIN = "college-football"
+PLATFORM = "sensor"
+ATTRIBUTION = "Data provided by ESPN"
+COORDINATOR = "coordinator"
+PLATFORMS = ["sensor"]
