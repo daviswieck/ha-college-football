0aebf3537589038e359fb2d15be48879878b760c
gopher_dave <79770840+daviswieck@users.noreply.github.com>
2022-05-10 09:27:25 -0500

    Create test_sensor.py

---
+++ tests/test_sensor.py
@@ -0,0 +1,20 @@
+"""Test College Football Sensor"""
+from pytest_homeassistant_custom_component.common import MockConfigEntry
+
+from custom_components.college_football.const import DOMAIN
+from tests.const import CONFIG_DATA
+
+
+async def test_sensor(hass):
+
+    entry = MockConfigEntry(
+        domain=DOMAIN,
+        title="College_Football",
+        data=CONFIG_DATA,
+    )
+
+    entry.add_to_hass(hass)
+    assert await hass.config_entries.async_setup(entry.entry_id)
+    await hass.async_block_till_done()
+
+    assert "college_football" in hass.config.components
