fa84050bc5039931c3ccb5cc8aab394d5c07e941
gopher_dave <79770840+daviswieck@users.noreply.github.com>
2022-08-30 11:10:33 -0500

    Update __init__.py

--- custom_components/college_football/__init__.py
+++ custom_components/college_football/__init__.py
@@ -1,7 +1,7 @@
 """ College Football Team Status """
 import logging
 import datetime  #test
-from datetime import timedelta import date
+from datetime import timedelta
 import arrow
 
 import aiohttp
