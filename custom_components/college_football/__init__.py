13637bd3fc33258035f6fef2c067f8bb9d20f365
gopher_dave <79770840+daviswieck@users.noreply.github.com>
2022-08-30 10:37:35 -0500

    Update __init__.py

--- custom_components/college_football/__init__.py
+++ custom_components/college_football/__init__.py
@@ -144,7 +144,7 @@ async def update_game(config) -> dict:
 async def async_get_state(config) -> dict:
     """Query API for status."""
     
-    today = datetime.today()
+    today = datetime.datetime.today()
     thismonth = today.strftime("%Y%m")
     
     values = {}
