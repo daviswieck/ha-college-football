13904f5b8235ba585cca07597e9c609b3d227478
gopher_dave <79770840+daviswieck@users.noreply.github.com>
2022-10-04 11:26:23 -0500

    Added RGB values
    Thanks @firstof9

--- custom_components/college_football/sensor.py
+++ custom_components/college_football/sensor.py
@@ -9,7 +9,7 @@ from homeassistant.core import HomeAssistant
 from homeassistant.helpers import config_validation as cv
 from homeassistant.helpers.update_coordinator import CoordinatorEntity
 from homeassistant.util import slugify
-from . import AlertsDataUpdateCoordinator
+from . import CollegeFootballDataUpdateCoordinator
 
 from .const import (
     ATTRIBUTION,
@@ -44,7 +44,7 @@ async def async_setup_platform(hass, config, async_add_entities, discovery_info=
         config.data = config
 
     # Setup the data coordinator
-    coordinator = AlertsDataUpdateCoordinator(
+    coordinator = CollegeFootballDataUpdateCoordinator(
         hass,
         config,
         config[CONF_TIMEOUT],
@@ -167,6 +167,7 @@ class CollegeFootballScoresSensor(CoordinatorEntity):
         attrs["team_homeaway"] = self.coordinator.data["team_homeaway"]
         attrs["team_logo"] = self.coordinator.data["team_logo"]
         attrs["team_colors"] = self.coordinator.data["team_colors"]
+        attrs["team_colors_rbg"] = self.team_colors(self.coordinator.data["team_colors"])
         attrs["team_score"] = self.coordinator.data["team_score"]
         attrs["team_rank"] = self.coordinator.data["team_rank"]
         attrs["team_win_probability"] = self.coordinator.data["team_win_probability"]
@@ -178,9 +179,14 @@ class CollegeFootballScoresSensor(CoordinatorEntity):
         attrs["opponent_homeaway"] = self.coordinator.data["opponent_homeaway"]
         attrs["opponent_logo"] = self.coordinator.data["opponent_logo"]
         attrs["opponent_colors"] = self.coordinator.data["opponent_colors"]
+        attrs["opponent_colors_rgb"] = self.team_colors(
+            self.coordinator.data["opponent_colors"]
+        )
         attrs["opponent_score"] = self.coordinator.data["opponent_score"]
         attrs["opponent_rank"] = self.coordinator.data["opponent_rank"]
-        attrs["opponent_win_probability"] = self.coordinator.data["opponent_win_probability"]
+        attrs["opponent_win_probability"] = self.coordinator.data[
+            "opponent_win_probability"
+        ]
         attrs["opponent_timeouts"] = self.coordinator.data["opponent_timeouts"]
         attrs["last_update"] = self.coordinator.data["last_update"]
 
@@ -190,3 +196,17 @@ class CollegeFootballScoresSensor(CoordinatorEntity):
     def available(self) -> bool:
         """Return if entity is available."""
         return self.coordinator.last_update_success
+
+
+    def team_colors(self, colors) -> tuple:
+        if colors is None:
+            return None
+        color_list = []
+        _LOGGER.debug("Colors: %s", colors[0])
+        color_list.append(list(self.hex_to_rgb(colors[0])))
+        color_list.append(list(self.hex_to_rgb(colors[1])))
+        return color_list
+
+    def hex_to_rgb(self, hexa) -> tuple:
+        hexa = hexa.lstrip("#")
+        return tuple(int(hexa[i : i + 2], 16) for i in (0, 2, 4))
